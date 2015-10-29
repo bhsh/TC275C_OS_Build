9483f32512723e5d3372135236b67371067154d4
XiYanpeng <yanpeng.xi@delphi.com>
2015-10-29 21:22:15 +0800

    add

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/core1_kernel_abstract.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/core1_kernel_abstract.h
@@ -22,8 +22,13 @@
 			                                               core1_thread_taskcallback();                         \
 			                                               core1_thread_termination();                          \
 		                                               }
-#define CORE1_PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize) PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize)  
+#define __CORE1_PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize) PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize)  
 
+#define CORE1_PTHREAD_CONTROL_BLOCK(thread_id)  __CORE1_PTHREAD_CONTROL_BLOCK(core1_os_th##thread_id, \
+	                                                                          CORE1_THREAD##thread_id##_PRIORITY, \
+	                                                                          SCHED_FIFO, \
+	                                                                          CORE1_THREAD##thread_id##_STACK_SIZE)
+	                                                                          
 #define __CORE1_PTHREAD_CREATION_BLOCK(thread_var,thread_attr,thread_name,thread_id,callback_task_name)  \
 	           pthread_create_np(thread_var,thread_attr,thread_name,thread_id,callback_task_name);
 
