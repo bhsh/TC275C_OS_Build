008bbd0d9b44559158f27f29f823d77a4f7c3dd8
XiYanpeng <yanpeng.xi@delphi.com>
2015-10-30 14:42:39 +0800

    add

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/os_kernel.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/os_kernel.h
@@ -8,7 +8,11 @@
 #ifndef OS_KERNEL_H_
 #define OS_KERNEL_H_
 
+/****************************************************************************/
+/* Feature Include Files                                                    */
+/****************************************************************************/
 #include "os_kernel_cfg.h"
+
 
 #define CORE0_PTHREAD_COND_INITIALIZER {CORE0,0,NULL}
 #define CORE1_PTHREAD_COND_INITIALIZER {CORE1,0,NULL}
@@ -16,11 +20,11 @@
 
 #define PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize) static struct { \
     pthread_t next,prev;\
-    uint32_t lcx; \
-    uint32_t priority; \
-    uint32_t policy; \
-    uint32_t *arg; \
-    uint32_t stack[_stacksize]; \
+    osu32_t lcx; \
+    osu32_t priority; \
+    osu32_t policy; \
+    osu32_t *arg; \
+    osu32_t stack[_stacksize]; \
     } _##_name = {0,(pthread_t)&_##_name,0,_priority,_policy,NULL,{_stacksize+1}};\
     \
     pthread_t _name = (pthread_t)&_##_name;
@@ -65,29 +69,29 @@ enum call_depth_overflow_e {
 //! Description of the thread attributes.
 typedef struct {
     enum user_e mode;//!< thread mode must be 0 user_e
-    uint32_t call_depth_overflow;//!< call depth overflow
+    osu32_t call_depth_overflow;//!< call depth overflow
 } pthread_attr_t;
 
 //! Description the thread record.
 typedef struct pthread_s {
     struct pthread_s *next; //!< Next thread pointer
     struct pthread_s *prev; //!< Previous thread pointer
-    uint32_t lcx; //!< Lower context pointer
-    uint32_t priority; //!< Thread priority must be 0 to \ref PTHREAD_PRIO_MAX
-    uint32_t policy; //!< Policy is one of sched_policy_e
-    uint32_t *arg; //!< Container that saves the argument passed to the thread function
-    uint32_t stack[1]; //!< Stack. The size 1 is only a dummy. Memory allocation is done via \ref PTHREAD_CONTROL_BLOCK
+    osu32_t lcx; //!< Lower context pointer
+    osu32_t priority; //!< Thread priority must be 0 to \ref PTHREAD_PRIO_MAX
+    osu32_t policy; //!< Policy is one of sched_policy_e
+    osu32_t *arg; //!< Container that saves the argument passed to the thread function
+    osu32_t stack[1]; //!< Stack. The size 1 is only a dummy. Memory allocation is done via \ref PTHREAD_CONTROL_BLOCK
 }*pthread_t;
 
 typedef struct {
-    const uint32_t core_id;//!< cond status is one of <true | false>
-    uint32_t  multi_semaphore;
+    const osu32_t core_id;//!< cond status is one of <true | false>
+    osu32_t  multi_semaphore;
     pthread_t blocked_threads; //!< list threads waiting for condition
 } pthread_cond_t;
 
 //! Description of a thread mutex.
 typedef struct {
-    uint32_t lock;//!< mutex lock status is one of <true | false>
+    osu32_t lock;//!< mutex lock status is one of <true | false>
     pthread_t owner; //!< owner thread
     pthread_t blocked_threads; //!< list threads waiting for mutex
 } pthread_mutex_t;
@@ -98,15 +102,15 @@ typedef struct {
 #include "os_type.h"
 #include "os.h"
 
-int pthread_create_np(pthread_t, const pthread_attr_t *, void(*)(void *,task_ptr_t),
+os32_t pthread_create_np(pthread_t, const pthread_attr_t *, void(*)(void *,task_ptr_t),
         void *,task_ptr_t);
 void start_core0_os(void);
 void start_core1_os(void);
 void start_core2_os(void);
 
-inline uint32_t os_getCoreId(void)
+inline osu32_t os_getCoreId(void)
 {
-   uint32_t core_id;
+   osu32_t core_id;
 
    core_id=__mfcr(CPU_CORE_ID);
    
@@ -115,13 +119,13 @@ inline uint32_t os_getCoreId(void)
 
 //! Start threads
 inline void pthread_start_np(void) {
-    extern  uint32_t  core0_os_pthread_runnable;
+    extern  osu32_t  core0_os_pthread_runnable;
     extern  pthread_t core0_os_pthread_running;
     extern  pthread_t core0_os_pthread_runnable_threads[PTHREAD_PRIO_MAX];
-	extern  uint32_t  core1_os_pthread_runnable;
+	extern  osu32_t  core1_os_pthread_runnable;
     extern  pthread_t core1_os_pthread_running;
     extern  pthread_t core1_os_pthread_runnable_threads[PTHREAD_PRIO_MAX];
-	extern  uint32_t  core2_os_pthread_runnable;
+	extern  osu32_t  core2_os_pthread_runnable;
     extern  pthread_t core2_os_pthread_running;
     extern  pthread_t core2_os_pthread_runnable_threads[PTHREAD_PRIO_MAX];
 	extern  allthreads_status_t core0_allthreads_status;
@@ -185,50 +189,50 @@ inline void pthread_start_np(void) {
 //!  TriCore context structure
 typedef union {
     struct {
-        uint32_t pcxi; //!< upper context PCXI
-        uint32_t psw; //!< upper context PSW
+        osu32_t pcxi; //!< upper context PCXI
+        osu32_t psw; //!< upper context PSW
         void * a10; //!< upper context A10 (SP)
         void * a11; //!< upper context A11 (RA)
-        uint32_t d8; //!< upper context D8
-        uint32_t d9; //!< upper context D9
-        uint32_t d10; //!< upper context D10
-        uint32_t d11; //!< upper context D11
-        uint32_t a12; //!< upper context A12
-        uint32_t a13; //!< upper context A13
-        uint32_t a14; //!< upper context A14
-        uint32_t a15; //!< upper context A15
-        uint32_t d12; //!< upper context D12
-        uint32_t d13; //!< upper context D13
-        uint32_t d14; //!< upper context D14
-        uint32_t d15; //!< upper context D15
+        osu32_t d8; //!< upper context D8
+        osu32_t d9; //!< upper context D9
+        osu32_t d10; //!< upper context D10
+        osu32_t d11; //!< upper context D11
+        osu32_t a12; //!< upper context A12
+        osu32_t a13; //!< upper context A13
+        osu32_t a14; //!< upper context A14
+        osu32_t a15; //!< upper context A15
+        osu32_t d12; //!< upper context D12
+        osu32_t d13; //!< upper context D13
+        osu32_t d14; //!< upper context D14
+        osu32_t d15; //!< upper context D15
     } u; //!< upper context
     struct {
-        uint32_t pcxi; //!< lower context PCXI
+        osu32_t pcxi; //!< lower context PCXI
         void (*pc)(void*,task_ptr_t); //!< lower context saved PC
-        uint32_t a2; //!< lower context A2
-        uint32_t a3; //!< lower context A3
-        uint32_t d0; //!< lower context D0
-        uint32_t d1; //!< lower context D1
-        uint32_t d2; //!< lower context D2
-        uint32_t d3; //!< lower context D3
+        osu32_t a2; //!< lower context A2
+        osu32_t a3; //!< lower context A3
+        osu32_t d0; //!< lower context D0
+        osu32_t d1; //!< lower context D1
+        osu32_t d2; //!< lower context D2
+        osu32_t d3; //!< lower context D3
         void * a4; //!< lower context A4
         void * a5; //!< lower context A5
-        uint32_t a6; //!< lower context A6
-        uint32_t a7; //!< lower context A7
-        uint32_t d4; //!< lower context D4
-        uint32_t d5; //!< lower context D5
-        uint32_t d6; //!< lower context D6
-        uint32_t d7; //!< lower context D7
+        osu32_t a6; //!< lower context A6
+        osu32_t a7; //!< lower context A7
+        osu32_t d4; //!< lower context D4
+        osu32_t d5; //!< lower context D5
+        osu32_t d6; //!< lower context D6
+        osu32_t d7; //!< lower context D7
     } l; //!< lower context
 } context_t;
 
-inline context_t *cx_to_addr(uint32_t cx) {
-    uint32_t seg_nr = __extru(cx, 16, 4);
+inline context_t *cx_to_addr(osu32_t cx) {
+    osu32_t seg_nr = __extru(cx, 16, 4);
     return (context_t *) __insert(seg_nr << 28, cx, 6, 16);
 }
 
 #define __fCPU 200
-inline void delay_ms(uint32_t _milliseconds) {
+inline void delay_ms(osu32_t _milliseconds) {
     __asm( "  mov.u d15,#1000 \n"
             "  mov.a a15,d15  \n"
             "  nop16          \n"
@@ -239,20 +243,20 @@ inline void delay_ms(uint32_t _milliseconds) {
 }
 
 //! Insert NEZ.A instruction
-inline uint32_t neza(void *p) {
-    int ret;
+inline osu32_t neza(void *p) {
+    os32_t ret;
     __asm("nez.a %0,%1":"=d"(ret):"a"(p));
     return ret;
 }
 
-/* The functions are called by app */
-int pthread_mutex_lock(pthread_mutex_t *mutex) ;//!<  [in] mutex pointer
-int pthread_mutex_unlock(pthread_mutex_t *mutex) ;
-//! Wait on a condition
-int pthread_cond_wait(pthread_cond_t *cond);//!< [in] condition pointer
-int pthread_cond_broadcast(pthread_cond_t *cond); //!< [in] condition pointer
-//! Wait on a condition
-int pthread_cond_timedwait_np(uint16_t reltime); //!< [in] relative time are the relative time STM_TIM4 ticks.NOT PORTABLE.
 
+/****************************************************************************/
+/* Function Prototype Definitions(The functions are called by threads)      */
+/****************************************************************************/
+os32_t pthread_mutex_lock(pthread_mutex_t *mutex); /* <*mutex> Mutex pointer*/
+os32_t pthread_mutex_unlock(pthread_mutex_t *mutex); /* <*mutex> Mutex pointer*/
+os32_t pthread_cond_wait(pthread_cond_t *cond); /* <*cond> Condition pointer*/
+os32_t pthread_cond_broadcast(pthread_cond_t *cond); /* <*cond> Condition pointer*/
+os32_t pthread_cond_timedwait_np(uint16_t reltime); /* <reltime> Waiting time ,unit:ms */
 
 #endif /* OS_KERNEL_H_ */
