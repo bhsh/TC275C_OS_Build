e4427d535bdd54d245e5821c82815e527752c5c5
XiYanpeng <yanpeng.xi@delphi.com>
2015-10-31 01:24:19 +0800

    add

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/kernel_callback_mapping.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/kernel_callback_mapping.h
@@ -1,12 +1,22 @@
-
-
+/****************************************************************************/
+/* FILE NAME:    kernel_callback_mapping.h                                  */
+/* CREATE ON:    Aug 26, 2015                                               */
+/* AUTHER:       Yanpeng.xi                                                 */
+/* DESCRIPTION:  The h file includes macro definitions and extern statements*/
+/*               that are intended to mapping the upper logic into os kernel*/
+/* COMMENT:      Multicore OS based on Aurix 275C app kit and TASKING 4.3   */
+/*               compiler                                                   */
+/****************************************************************************/
 #ifndef KERNEL_CALLBACK_MAPPING_H_ 
 #define KERNEL_CALLBACK_MAPPING_H_
-
 
 #define  PTHREAD_OBTAIN_INIT_STACK_ADD_CALLBACK(core_id,thread_id,init_sp_address) \
 	             get_thread_init_stack_address(core_id,thread_id,init_sp_address);
 
+#define  PTHREAD_OBTAIN_TIMESLOT(current_core_id,thread_id) \
+	             os_trace_thread_timeslot(current_core_id,thread_id)
+
 extern void get_thread_init_stack_address(osu32_t,osu32_t,osu32_t);
+extern void os_trace_thread_timeslot(osu32_t ,osu32_t);
 
 #endif /* End of KERNEL_CALLBACK_MAPPING_H_ */
--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/os_trace.c
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/os_trace.c
@@ -1,17 +1,25 @@
-
+/****************************************************************************/
+/* FILE NAME:    os_trace.c                                                 */
+/* CREATE ON:    Aug 26, 2015                                               */
+/* AUTHER:       Yanpeng.xi                                                 */
+/* DESCRIPTION:  The c file includes logic that traces the time             */
+/*               characteristics of tasks                                   */
+/*               tasks                                                      */
+/* COMMENT:      Multicore OS based on Aurix 275C app kit and TASKING 4.3   */
+/*               compiler                                                   */
+/****************************************************************************/
 #include "os_type.h"
 #include "os_interface.h"
 
+#define TRACE_MAX_CORE_NUM    (3)
+#define TRACE_MAX_THREAD_NUM  (11)
 
-#define MAX_CORE_NUM    (3)
-#define MAX_THREAD_NUM  (11)
-
-static volatile osu32_t os_thread_time[MAX_CORE_NUM][MAX_THREAD_NUM];
-static volatile osu32_t os_tick_begin[MAX_CORE_NUM][MAX_THREAD_NUM];
-static volatile osu32_t os_thread_timeslot[MAX_CORE_NUM][MAX_THREAD_NUM];
-static volatile osu32_t os_thread_switch_times[MAX_CORE_NUM][MAX_THREAD_NUM];
-static volatile osu32_t os_thread_begin_timeslot[MAX_CORE_NUM];
-static volatile osu32_t os_thread_end_timeslot[MAX_CORE_NUM];
+static volatile osu32_t os_thread_time[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
+static volatile osu32_t os_tick_begin[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
+static volatile osu32_t os_thread_timeslot[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
+static volatile osu32_t os_thread_switch_times[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
+static volatile osu32_t os_thread_begin_timeslot[TRACE_MAX_CORE_NUM];
+static volatile osu32_t os_thread_end_timeslot[TRACE_MAX_CORE_NUM];
 
 static osu32_t os_trace_time(void)
 {
@@ -38,7 +46,6 @@ void os_trace_task_time_end(osu32_t current_core_id,osu32_t thread_id)
   os_thread_time[current_core_id][thread_id] = 
   	   os_trace_time() - os_tick_begin[current_core_id][thread_id];
 }
-
 void os_trace_thread_timeslot(osu32_t current_core_id,osu32_t thread_id)
 {
   os_thread_timeslot[current_core_id][thread_id] = os_trace_time();  
