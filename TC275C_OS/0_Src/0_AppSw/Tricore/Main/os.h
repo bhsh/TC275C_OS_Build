9c5d6166a3b11d061b4c6aa139def25040e383aa
XiYanpeng <yanpeng.xi@delphi.com>
2015-10-31 16:50:54 +0800

    add

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/os.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/os.h
@@ -8,7 +8,7 @@ typedef struct {
 	const osu32_t curr_task_id;
     osu32_t       curr_task_type;
     osu32_t       curr_task_period;
-    osu32_t       curr_task_order_num;
+    const osu32_t curr_task_order_num;
     osu32_t       actived_task_core_id;
 	osu32_t       actived_task_id;
     
