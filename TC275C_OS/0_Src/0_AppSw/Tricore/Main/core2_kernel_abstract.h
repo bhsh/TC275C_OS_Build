5a5900591dcf3bf25a958805a2103559bf72aded
XiYanpeng <yanpeng.xi@delphi.com>
2015-10-31 09:27:49 +0800

    ADD

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/core2_kernel_abstract.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/core2_kernel_abstract.h
@@ -5,36 +5,42 @@
 #include "os_kernel.h"
 
 
-#define core2_thread_initialization();    pthread_config_t pthread_config =                                  \
-	                                      core2_pthread_init_config_database[(int)arg];                      \
-                                          for (;;)                                                           \
-                                          {                                                                  \
-								 		  	core2_thread_done_before_task(&pthread_config); 
-#define core2_thread_taskcallback();        task(&pthread_config);          	                                         
-#define core2_thread_termination();         core2_thread_done_after_task(&pthread_config);                   \
-	                                      }
+#define CORE2_PTHREAD_INITIALIZATION_BLOCK  \
+	pthread_config_t pthread_config = \
+	core2_pthread_init_config_database[(int)arg]; \
+    for (;;){ \
+	  core2_thread_done_before_task(&pthread_config); 
+	
+#define CORE2_PTHREAD_TASKCALLBACK_BLOCK task(&pthread_config);   
 
-#define CORE2_PTHREAD_DEFINITION_BLOCK(thread_id)    void core2_os_thread##thread_id(void* arg,task_ptr_t task) \
-			                                           {                                                        \
-			                                               core2_thread_initialization();                       \
-			                                               core2_thread_taskcallback();                         \
-			                                               core2_thread_termination();                          \
-		                                               }
-#define __CORE2_PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize) PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize)  
+#define CORE2_PTHREAD_TERMINATION_BLOCK  \
+	  core2_thread_done_after_task(&pthread_config);}
 
-#define CORE2_PTHREAD_CONTROL_BLOCK(thread_id)  __CORE2_PTHREAD_CONTROL_BLOCK(core2_os_th##thread_id, \
-	                                                                          CORE2_THREAD##thread_id##_PRIORITY, \
-	                                                                          SCHED_FIFO, \
-	                                                                          CORE2_THREAD##thread_id##_STACK_SIZE)
+#define CORE2_PTHREAD_DEFINITION_BLOCK(thread_id)  \
+	void core2_os_thread##thread_id(void* arg,task_ptr_t task){ \
+	    CORE2_PTHREAD_INITIALIZATION_BLOCK  \
+	    CORE2_PTHREAD_TASKCALLBACK_BLOCK  \
+	    CORE2_PTHREAD_TERMINATION_BLOCK}
+
+#define __CORE2_PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize) \
+	PTHREAD_CONTROL_BLOCK(_name,_priority,_policy,_stacksize)  
+
+#define CORE2_PTHREAD_CONTROL_BLOCK(thread_id) \
+	__CORE2_PTHREAD_CONTROL_BLOCK(core2_os_th##thread_id, \
+	                              CORE2_THREAD##thread_id##_PRIORITY, \
+	                              SCHED_FIFO, \
+	                              CORE2_THREAD##thread_id##_STACK_SIZE)
 
 #define __CORE2_PTHREAD_CREATION_BLOCK(thread_var,thread_attr,thread_name,thread_id,callback_task_name)  \
-	           pthread_create_np(thread_var,thread_attr,thread_name,thread_id,callback_task_name);
+	 pthread_create_np(thread_var,thread_attr,thread_name,thread_id,callback_task_name);
 
-#define CORE2_PTHREAD_CREATION_BLOCK(thread_id)  __CORE2_PTHREAD_CREATION_BLOCK(core2_os_th##thread_id, \
-		                                                                        &core2_thread_attr[CORE2_THREAD_ID##thread_id],\
-		                                                                        core2_os_thread##thread_id,\
-		                                                                        (void*) CORE2_THREAD_ID##thread_id, \
-		                                                                        CORE2_TASK##thread_id)
+#define CORE2_PTHREAD_CREATION_BLOCK(thread_id) \
+	__CORE2_PTHREAD_CREATION_BLOCK(core2_os_th##thread_id, \
+		                           &core2_thread_attr[CORE2_THREAD_ID##thread_id],\
+		                           core2_os_thread##thread_id,\
+		                           (void*) CORE2_THREAD_ID##thread_id, \
+		                           CORE2_TASK##thread_id)
+		                           
 #define CORE2_PTHREAD_START_BLOCK() pthread_start_np();
 
 #endif
