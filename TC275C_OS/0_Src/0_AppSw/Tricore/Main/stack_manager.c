2d2a7450f586d9c6deb7384684b5904224320894
XiYanpeng <yanpeng.xi@delphi.com>
2015-11-01 10:20:10 +0800

    ADD

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/stack_manager.c
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/stack_manager.c
@@ -3,20 +3,29 @@
 /* CREATE ON:    Aug 26, 2015                                               */
 /* AUTHER:       Yanpeng.xi                                                 */
 /* DESCRIPTION:  The c file includes logic that tests the stack usage of    */
-/*               tasks                                                      */
+/*               each task in thread                                        */
 /* COMMENT:      Multicore OS based on Aurix 275C app kit and TASKING 4.3   */
 /*               compiler                                                   */
+/****************************************************************************/
+
+/****************************************************************************/
+/* Feature Include Files                                                    */
 /****************************************************************************/
 #include "os_type.h"
 #include "low_driver_port.h"
 
+/****************************************************************************/
+/* Macro Definitions                                                        */
+/****************************************************************************/
 #define TIME_UNIT_SWITCH            (1000)
 #define STACK_MEA_MAX_POS_NUM       (10)
 #define PTHREAD_STACK_SIZE_IN_BYTE  (256)
-
 #define STACK_MEA_MAX_CORE_NUM      (3)
 #define STACK_MEA_MAX_THREAD_NUM    (11)
 
+/****************************************************************************/
+/* Type Definitions                                                         */
+/****************************************************************************/
 typedef struct 
 {
     osu32_t stack_init_address;
@@ -26,8 +35,16 @@ typedef struct
 	
 } pthread_stack_t; 
 
+/****************************************************************************/
+/* Static Variable Definitions                                              */
+/****************************************************************************/
 static pthread_stack_t pthread_stack[STACK_MEA_MAX_CORE_NUM][STACK_MEA_MAX_THREAD_NUM][STACK_MEA_MAX_POS_NUM];
 
+/****************************************************************************/
+/* FUNTION NAME:get_stack_used_percent                                      */
+/* DESCRIPTION: Get percent of stack usage for each task                    */
+/* RETURN RESOLUTION : 100% - 1000                                          */
+/****************************************************************************/
 osu32_t get_stack_used_percent(osu32_t thread_id, osu32_t pos)
 {
 	osu32_t current_cpu_id = LowDriver_Get_Curr_Core_ID();
@@ -42,8 +59,12 @@ osu32_t get_stack_used_percent(osu32_t thread_id, osu32_t pos)
             (pthread_stack[current_cpu_id][thread_id][pos].stack_used_size_byte*TIME_UNIT_SWITCH)/PTHREAD_STACK_SIZE_IN_BYTE;
 
 	return pthread_stack[current_cpu_id][thread_id][pos].stack_used_percent;
-}
+} /* End of get_stack_used_percent function */
 
+/****************************************************************************/
+/* FUNTION NAME:get_thread_init_stack_address                               */
+/* DESCRIPTION: Get the initial stack address of every thread               */
+/****************************************************************************/
 void get_thread_init_stack_address(osu32_t core_id,osu32_t thread_id,osu32_t init_sp_address)
 {  
    osu32_t index;
@@ -55,5 +76,5 @@ void get_thread_init_stack_address(osu32_t core_id,osu32_t thread_id,osu32_t ini
        pthread_stack[core_id][thread_id][index].stack_init_address = init_sp_address;
      }
    }
-}
+} /* End of get_thread_init_stack_address function */
 
--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/stack_manager.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/stack_manager.h
@@ -1,7 +1,18 @@
-
+/****************************************************************************/
+/* FILE NAME:    stack_manager.h                                            */
+/* CREATE ON:    Aug 26, 2015                                               */
+/* AUTHER:       Yanpeng.xi                                                 */
+/* DESCRIPTION:  The h file includes logic that measure the stack usages of */
+/*               every each task in thread                                  */
+/* COMMENT:      Multicore OS based on Aurix 275C app kit and TASKING 4.3   */
+/*               compiler                                                   */
+/****************************************************************************/
 #ifndef  STACK_MANAGER_H_  
 #define  STACK_MANAGER_H_
 
+/****************************************************************************/
+/* Function Prototype Definitions                                           */
+/****************************************************************************/
 OS_EXTERN osu32_t get_stack_used_percent(osu32_t task_id, osu32_t pos);
 
 #endif /* End of STACK_MANAGER_H_ */
