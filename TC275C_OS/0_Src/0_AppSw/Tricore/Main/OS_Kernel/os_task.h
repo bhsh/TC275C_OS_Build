8364390c1fc809a027e2df729b103f09d6374205
XiYanpeng <yanpeng.xi@delphi.com>
2015-11-01 16:58:32 +0800

    add

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/OS_Kernel/os_task.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/OS_Kernel/os_task.h
@@ -15,39 +15,107 @@
 #include "os_kernel.h"
 
 /****************************************************************************/
-/* Macro Definitions                                                        */
+/* Type Definitions                                                        */
 /****************************************************************************/
+typedef pthread_mutex_t task_mutex_t;
+typedef pthread_cond_t  task_cond_t;
+
+/****************************************************************************/
+/* User APIs                                                                */
+/****************************************************************************/
+
 /****************************************************************************/
 /* MACRO NAME: Task_Mutex_Lock                                              */
-/* FUNTION PROTOTYPE:  Task_Mutex_Lock(pthread_mutex_t *mutex)                                                       */
-/* DESCRIPTION:                                                             */
+/* FUNCTION PROTOTYPE:  Task_Mutex_Lock(task_mutex_t *mutex)                */
+/* ARGUMENT NAME: mutex                                                     */
+/* ARGUMENT TYPE: task_mutex_t *                                            */
+/* RETURN NAME :  None                                                      */
+/* RETURN TYPE :  None                                                      */
+/* USING RANGE :  Inside tasks                                              */
+/****************************************************************************/
+/* USAGE DESCRIPTION :                                                      */
+/*                                                                          */
+/*   task_mutex_t  mutex;                                                   */
+/*   ......                                                                 */
+/*   Task_Mutex_Lock(&mutex);                                               */
+/*   // Your critical source that can only belong to                        */
+/*   // one user at one time                                                */           
+/*   Task_Mutex_Unlock(&mutex);                                             */
+/*   ......                                                                 */
 /****************************************************************************/
 #define Task_Mutex_Lock(mutex_ptr)          pthread_mutex_lock(mutex_ptr)
 
 /****************************************************************************/
-/* MACRO NAME: Task_Mutex_Lock()                                              */
-/* DESCRIPTION:                                                             */
-/* DESCRIPTION:                                                             */
-/* DESCRIPTION:                                                             */
-/* DESCRIPTION:                                                             */
+/* MACRO NAME: Task_Mutex_Unlock                                            */
+/* FUNCTION PROTOTYPE:  Task_Mutex_Unlock(task_mutex_t *mutex)              */
+/* ARGUMENT NAME: mutex                                                     */
+/* ARGUMENT TYPE: task_mutex_t *                                            */
+/* RETURN NAME :  None                                                      */
+/* RETURN TYPE :  None                                                      */
+/* USING RANGE :  Inside tasks                                              */
+/****************************************************************************/
+/* USAGE DESCRIPTION :  Please refer to Task_Mutex_Lock                     */
 /****************************************************************************/
 #define Task_Mutex_Unlock(mutex_ptr)        pthread_mutex_lock(mutex_ptr)
 
 /****************************************************************************/
-/* MACRO NAME: Task_Mutex_Lock                                              */
-/* DESCRIPTION:                                                             */
+/* MACRO NAME: Task_Cond_Wait                                               */
+/* FUNCTION PROTOTYPE:  Task_Cond_Wait(task_cond_t *cond)                   */
+/* ARGUMENT NAME: cond                                                      */
+/* ARGUMENT TYPE: task_cond_t *                                             */
+/* RETURN NAME :  None                                                      */
+/* RETURN TYPE :  None                                                      */
+/* USING RANGE :  Inside tasks                                              */
+/****************************************************************************/
+/* USAGE DESCRIPTION :                                                      */
+/*                                                                          */
+/*   task_cond_t  cond;                                                     */
+/*   ......                                                                 */
+/*   Task_Cond_Wait(&cond);                                                 */
+/*   // The current task has been blocked when Task_Cond_Wait is reached    */ 
+/*   // The following code of the task can only be executed after the task  */
+/*   // is actived by Task_Cond_Broadcast                                   */
+/*   ......                                                                 */
 /****************************************************************************/
 #define Task_Cond_Wait(cond_ptr)            pthread_cond_wait(cond_ptr)
 
 /****************************************************************************/
-/* MACRO NAME: Task_Mutex_Lock                                              */
-/* DESCRIPTION:                                                             */
+/* MACRO NAME: Task_Cond_Broadcast                                          */
+/* FUNCTION PROTOTYPE:  Task_Cond_Broadcast(task_cond_t *cond)              */
+/* ARGUMENT NAME: cond                                                      */
+/* ARGUMENT TYPE: task_cond_t *                                             */
+/* RETURN NAME :  None                                                      */
+/* RETURN TYPE :  None                                                      */
+/* USING RANGE :  Inside tasks or interrupt                                 */
+/****************************************************************************/
+/* USAGE DESCRIPTION : The API can actives tasks that have been blocked by  */
+/*                     Task_Cond_Wait.                                      */
+/*                     Here Task_Cond_Wait and Task_Cond_Broadcast should   */
+/*                     use the same cond                                    */
+/*                     Task_Cond_Broadcast can also be use to active task of*/
+/*                     another core os                                      */
+/*   task_cond_t  cond;                                                     */
+/*   ......                                                                 */
+/*   Task_Cond_Broadcast(&cond);                                            */
+/*   ......                                                                 */
 /****************************************************************************/
 #define Task_Cond_Broadcast(cond_ptr)       pthread_cond_broadcast(cond_ptr)
 
 /****************************************************************************/
-/* MACRO NAME: Task_Mutex_Lock                                              */
-/* DESCRIPTION:                                                             */
+/* MACRO NAME: Task_Cond_Timedwait                                          */
+/* FUNCTION PROTOTYPE:  Task_Cond_Timedwait(usigned short wait_time)        */
+/* ARGUMENT NAME: wait_time                                                 */
+/* ARGUMENT TYPE: usigned short                                             */
+/* RETURN NAME :  None                                                      */
+/* RETURN TYPE :  None                                                      */
+/* USING RANGE :  Inside tasks                                              */
+/****************************************************************************/
+/* USAGE DESCRIPTION : The API can active the task for wait_time ms.When    */
+/*                     the task is blocked, the task owns lower priority    */
+/*                     can be executed                                      */
+/*   ......                                                                 */
+/*   Task_Cond_Timedwait(500); // Wait 500ms                                */
+/*   ......                                                                 */
 /****************************************************************************/
 #define Task_Cond_Timedwait(wait_time)      pthread_cond_timedwait_np(wait_time)
 
