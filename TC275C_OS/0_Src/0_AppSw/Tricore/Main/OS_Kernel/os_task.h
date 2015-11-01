1b6d50d670e5d654868ad75511fbc4fe891aa71b
XiYanpeng <yanpeng.xi@delphi.com>
2015-11-01 16:08:13 +0800

    ADD

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/OS_Kernel/os_task.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/OS_Kernel/os_task.h
@@ -10,20 +10,72 @@
 #define OS_TASK_H_
 
 /****************************************************************************/
-/* Feature Include Files                                                    */
+/* Macro Definitions                                                        */
 /****************************************************************************/
 
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Mutex_Lock(mutex_ptr)          pthread_mutex_lock(mutex_ptr)
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Mutex_Unlock(mutex_ptr)        pthread_mutex_lock(mutex_ptr)
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Cond_Wait(cond_ptr)            pthread_cond_wait(cond_ptr)
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Cond_Broadcast(cond_ptr)       pthread_cond_broadcast(cond_ptr)
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Cond_Timedwait(wait_time)      pthread_cond_timedwait_np(wait_time)
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Enable_AllInterrupts()         pthread_enable_allinterrupts()
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Disable_AllInterrupts()        pthread_enable_allinterrupts()
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Suspend_AllThreads()           pthread_suspend_allthreads()
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Restore_AllThreads()           pthread_restore_allthreads()
+
+/****************************************************************************/
+/* MACRO NAME: Task_Mutex_Lock                                              */
+/* DESCRIPTION:                                                             */
+/****************************************************************************/
 #define Task_Obtain_os_tick(cpu_id)         pthread_obtain_os_tick(cpu_id)
 
+/****************************************************************************/
+/* Feature Include Files                                                    */
+/****************************************************************************/
 #include "os_kernel.h"
 
 #endif /* End of OS_TASK_H_ */
