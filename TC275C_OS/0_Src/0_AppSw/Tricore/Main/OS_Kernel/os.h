b8037465310bf4df922840e2fe3e0bde0beb7994
XiYanpeng <yanpeng.xi@delphi.com>
2015-11-01 18:00:00 +0800

    add

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/OS_Kernel/os.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/OS_Kernel/os.h
@@ -27,6 +27,8 @@ typedef struct {
     
 } pthread_config_t;
 
+/* <typedef pthread_config_t* task_cfg_t><pthread_config_t> The type is used*/
+/* by interface the thread type to task type for user                       */
 typedef pthread_config_t* task_cfg_t;
 
 /* <Function pointer><task_ptr_t> The type is used by thread creation API to*/
