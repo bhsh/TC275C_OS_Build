4e2d175e4f20afe056634a5b594dc55ee38227f3
XiYanpeng <yanpeng.xi@delphi.com>
2015-11-01 17:56:29 +0800

    add

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/Core1OS/core1_tasks.c
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/Core1OS/core1_tasks.c
@@ -15,143 +15,143 @@
 /****************************************************************************/
 /* <CORE1> Task Definitions                                                 */
 /****************************************************************************/
-void CORE1_TASK0(pthread_config_t *pthread_config)
+void CORE1_TASK0(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
-  App_shared_func_stack_background_count(pthread_config->curr_task_core_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
+  App_shared_func_stack_background_count(task_cfg->curr_task_core_id);
   App_priv1_func_test_count();
 }
-void CORE1_TASK1(pthread_config_t *pthread_config)
+void CORE1_TASK1(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
   //App_shared_func_flash_led_1();
 }
-void CORE1_TASK2(pthread_config_t *pthread_config)
+void CORE1_TASK2(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
   //App_shared_func_flash_led_2();
 }
-void CORE1_TASK3(pthread_config_t *pthread_config)
+void CORE1_TASK3(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE1_TASK4(pthread_config_t *pthread_config)
+void CORE1_TASK4(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE1_TASK5(pthread_config_t *pthread_config)
+void CORE1_TASK5(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE1_TASK6(pthread_config_t *pthread_config)
+void CORE1_TASK6(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE1_TASK7(pthread_config_t *pthread_config)
+void CORE1_TASK7(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE1_TASK8(pthread_config_t *pthread_config)
+void CORE1_TASK8(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE1_TASK9(pthread_config_t *pthread_config)
+void CORE1_TASK9(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE1_TASK10(pthread_config_t *pthread_config)
+void CORE1_TASK10(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
-  App_shared_func_stack_calculated(pthread_config->curr_task_core_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
+  App_shared_func_stack_calculated(task_cfg->curr_task_core_id);
   //App_shared_func_flash_led_4();
 }
-void CORE1_TASK11(pthread_config_t *pthread_config){}
-void CORE1_TASK12(pthread_config_t *pthread_config){}
-void CORE1_TASK13(pthread_config_t *pthread_config){}
-void CORE1_TASK14(pthread_config_t *pthread_config){}
-void CORE1_TASK15(pthread_config_t *pthread_config){}
-void CORE1_TASK16(pthread_config_t *pthread_config){}
-void CORE1_TASK17(pthread_config_t *pthread_config){}
-void CORE1_TASK18(pthread_config_t *pthread_config){}
-void CORE1_TASK19(pthread_config_t *pthread_config){}
-void CORE1_TASK20(pthread_config_t *pthread_config){}
-void CORE1_TASK21(pthread_config_t *pthread_config){}
-void CORE1_TASK22(pthread_config_t *pthread_config){}
-void CORE1_TASK23(pthread_config_t *pthread_config){}
-void CORE1_TASK24(pthread_config_t *pthread_config){}
-void CORE1_TASK25(pthread_config_t *pthread_config){}
-void CORE1_TASK26(pthread_config_t *pthread_config){}
-void CORE1_TASK27(pthread_config_t *pthread_config){}
-void CORE1_TASK28(pthread_config_t *pthread_config){}
-void CORE1_TASK29(pthread_config_t *pthread_config){}
-void CORE1_TASK30(pthread_config_t *pthread_config){}
-void CORE1_TASK31(pthread_config_t *pthread_config){}
-void CORE1_TASK32(pthread_config_t *pthread_config){}
-void CORE1_TASK33(pthread_config_t *pthread_config){}
-void CORE1_TASK34(pthread_config_t *pthread_config){}
-void CORE1_TASK35(pthread_config_t *pthread_config){}
-void CORE1_TASK36(pthread_config_t *pthread_config){}
-void CORE1_TASK37(pthread_config_t *pthread_config){}
-void CORE1_TASK38(pthread_config_t *pthread_config){}
-void CORE1_TASK39(pthread_config_t *pthread_config){}
-void CORE1_TASK40(pthread_config_t *pthread_config){}
-void CORE1_TASK41(pthread_config_t *pthread_config){}
-void CORE1_TASK42(pthread_config_t *pthread_config){}
-void CORE1_TASK43(pthread_config_t *pthread_config){}
-void CORE1_TASK44(pthread_config_t *pthread_config){}
-void CORE1_TASK45(pthread_config_t *pthread_config){}
-void CORE1_TASK46(pthread_config_t *pthread_config){}
-void CORE1_TASK47(pthread_config_t *pthread_config){}
-void CORE1_TASK48(pthread_config_t *pthread_config){}
-void CORE1_TASK49(pthread_config_t *pthread_config){}
-void CORE1_TASK50(pthread_config_t *pthread_config){}
-void CORE1_TASK51(pthread_config_t *pthread_config){}
-void CORE1_TASK52(pthread_config_t *pthread_config){}
-void CORE1_TASK53(pthread_config_t *pthread_config){}
-void CORE1_TASK54(pthread_config_t *pthread_config){}
-void CORE1_TASK55(pthread_config_t *pthread_config){}
-void CORE1_TASK56(pthread_config_t *pthread_config){}
-void CORE1_TASK57(pthread_config_t *pthread_config){}
-void CORE1_TASK58(pthread_config_t *pthread_config){}
-void CORE1_TASK59(pthread_config_t *pthread_config){}
-void CORE1_TASK60(pthread_config_t *pthread_config){}
-void CORE1_TASK61(pthread_config_t *pthread_config){}
-void CORE1_TASK62(pthread_config_t *pthread_config){}
-void CORE1_TASK63(pthread_config_t *pthread_config){}
-void CORE1_TASK64(pthread_config_t *pthread_config){}
-void CORE1_TASK65(pthread_config_t *pthread_config){}
-void CORE1_TASK66(pthread_config_t *pthread_config){}
-void CORE1_TASK67(pthread_config_t *pthread_config){}
-void CORE1_TASK68(pthread_config_t *pthread_config){}
-void CORE1_TASK69(pthread_config_t *pthread_config){}
-void CORE1_TASK70(pthread_config_t *pthread_config){}
-void CORE1_TASK71(pthread_config_t *pthread_config){}
-void CORE1_TASK72(pthread_config_t *pthread_config){}
-void CORE1_TASK73(pthread_config_t *pthread_config){}
-void CORE1_TASK74(pthread_config_t *pthread_config){}
-void CORE1_TASK75(pthread_config_t *pthread_config){}
-void CORE1_TASK76(pthread_config_t *pthread_config){}
-void CORE1_TASK77(pthread_config_t *pthread_config){}
-void CORE1_TASK78(pthread_config_t *pthread_config){}
-void CORE1_TASK79(pthread_config_t *pthread_config){}
-void CORE1_TASK80(pthread_config_t *pthread_config){}
-void CORE1_TASK81(pthread_config_t *pthread_config){}
-void CORE1_TASK82(pthread_config_t *pthread_config){}
-void CORE1_TASK83(pthread_config_t *pthread_config){}
-void CORE1_TASK84(pthread_config_t *pthread_config){}
-void CORE1_TASK85(pthread_config_t *pthread_config){}
-void CORE1_TASK86(pthread_config_t *pthread_config){}
-void CORE1_TASK87(pthread_config_t *pthread_config){}
-void CORE1_TASK88(pthread_config_t *pthread_config){}
-void CORE1_TASK89(pthread_config_t *pthread_config){}
-void CORE1_TASK90(pthread_config_t *pthread_config){}
-void CORE1_TASK91(pthread_config_t *pthread_config){}
-void CORE1_TASK92(pthread_config_t *pthread_config){}
-void CORE1_TASK93(pthread_config_t *pthread_config){}
-void CORE1_TASK94(pthread_config_t *pthread_config){}
-void CORE1_TASK95(pthread_config_t *pthread_config){}
-void CORE1_TASK96(pthread_config_t *pthread_config){}
-void CORE1_TASK97(pthread_config_t *pthread_config){}
-void CORE1_TASK98(pthread_config_t *pthread_config){}
-void CORE1_TASK99(pthread_config_t *pthread_config){}
-void CORE1_TASK100(pthread_config_t *pthread_config){}
+void CORE1_TASK11(task_cfg_t task_cfg){}
+void CORE1_TASK12(task_cfg_t task_cfg){}
+void CORE1_TASK13(task_cfg_t task_cfg){}
+void CORE1_TASK14(task_cfg_t task_cfg){}
+void CORE1_TASK15(task_cfg_t task_cfg){}
+void CORE1_TASK16(task_cfg_t task_cfg){}
+void CORE1_TASK17(task_cfg_t task_cfg){}
+void CORE1_TASK18(task_cfg_t task_cfg){}
+void CORE1_TASK19(task_cfg_t task_cfg){}
+void CORE1_TASK20(task_cfg_t task_cfg){}
+void CORE1_TASK21(task_cfg_t task_cfg){}
+void CORE1_TASK22(task_cfg_t task_cfg){}
+void CORE1_TASK23(task_cfg_t task_cfg){}
+void CORE1_TASK24(task_cfg_t task_cfg){}
+void CORE1_TASK25(task_cfg_t task_cfg){}
+void CORE1_TASK26(task_cfg_t task_cfg){}
+void CORE1_TASK27(task_cfg_t task_cfg){}
+void CORE1_TASK28(task_cfg_t task_cfg){}
+void CORE1_TASK29(task_cfg_t task_cfg){}
+void CORE1_TASK30(task_cfg_t task_cfg){}
+void CORE1_TASK31(task_cfg_t task_cfg){}
+void CORE1_TASK32(task_cfg_t task_cfg){}
+void CORE1_TASK33(task_cfg_t task_cfg){}
+void CORE1_TASK34(task_cfg_t task_cfg){}
+void CORE1_TASK35(task_cfg_t task_cfg){}
+void CORE1_TASK36(task_cfg_t task_cfg){}
+void CORE1_TASK37(task_cfg_t task_cfg){}
+void CORE1_TASK38(task_cfg_t task_cfg){}
+void CORE1_TASK39(task_cfg_t task_cfg){}
+void CORE1_TASK40(task_cfg_t task_cfg){}
+void CORE1_TASK41(task_cfg_t task_cfg){}
+void CORE1_TASK42(task_cfg_t task_cfg){}
+void CORE1_TASK43(task_cfg_t task_cfg){}
+void CORE1_TASK44(task_cfg_t task_cfg){}
+void CORE1_TASK45(task_cfg_t task_cfg){}
+void CORE1_TASK46(task_cfg_t task_cfg){}
+void CORE1_TASK47(task_cfg_t task_cfg){}
+void CORE1_TASK48(task_cfg_t task_cfg){}
+void CORE1_TASK49(task_cfg_t task_cfg){}
+void CORE1_TASK50(task_cfg_t task_cfg){}
+void CORE1_TASK51(task_cfg_t task_cfg){}
+void CORE1_TASK52(task_cfg_t task_cfg){}
+void CORE1_TASK53(task_cfg_t task_cfg){}
+void CORE1_TASK54(task_cfg_t task_cfg){}
+void CORE1_TASK55(task_cfg_t task_cfg){}
+void CORE1_TASK56(task_cfg_t task_cfg){}
+void CORE1_TASK57(task_cfg_t task_cfg){}
+void CORE1_TASK58(task_cfg_t task_cfg){}
+void CORE1_TASK59(task_cfg_t task_cfg){}
+void CORE1_TASK60(task_cfg_t task_cfg){}
+void CORE1_TASK61(task_cfg_t task_cfg){}
+void CORE1_TASK62(task_cfg_t task_cfg){}
+void CORE1_TASK63(task_cfg_t task_cfg){}
+void CORE1_TASK64(task_cfg_t task_cfg){}
+void CORE1_TASK65(task_cfg_t task_cfg){}
+void CORE1_TASK66(task_cfg_t task_cfg){}
+void CORE1_TASK67(task_cfg_t task_cfg){}
+void CORE1_TASK68(task_cfg_t task_cfg){}
+void CORE1_TASK69(task_cfg_t task_cfg){}
+void CORE1_TASK70(task_cfg_t task_cfg){}
+void CORE1_TASK71(task_cfg_t task_cfg){}
+void CORE1_TASK72(task_cfg_t task_cfg){}
+void CORE1_TASK73(task_cfg_t task_cfg){}
+void CORE1_TASK74(task_cfg_t task_cfg){}
+void CORE1_TASK75(task_cfg_t task_cfg){}
+void CORE1_TASK76(task_cfg_t task_cfg){}
+void CORE1_TASK77(task_cfg_t task_cfg){}
+void CORE1_TASK78(task_cfg_t task_cfg){}
+void CORE1_TASK79(task_cfg_t task_cfg){}
+void CORE1_TASK80(task_cfg_t task_cfg){}
+void CORE1_TASK81(task_cfg_t task_cfg){}
+void CORE1_TASK82(task_cfg_t task_cfg){}
+void CORE1_TASK83(task_cfg_t task_cfg){}
+void CORE1_TASK84(task_cfg_t task_cfg){}
+void CORE1_TASK85(task_cfg_t task_cfg){}
+void CORE1_TASK86(task_cfg_t task_cfg){}
+void CORE1_TASK87(task_cfg_t task_cfg){}
+void CORE1_TASK88(task_cfg_t task_cfg){}
+void CORE1_TASK89(task_cfg_t task_cfg){}
+void CORE1_TASK90(task_cfg_t task_cfg){}
+void CORE1_TASK91(task_cfg_t task_cfg){}
+void CORE1_TASK92(task_cfg_t task_cfg){}
+void CORE1_TASK93(task_cfg_t task_cfg){}
+void CORE1_TASK94(task_cfg_t task_cfg){}
+void CORE1_TASK95(task_cfg_t task_cfg){}
+void CORE1_TASK96(task_cfg_t task_cfg){}
+void CORE1_TASK97(task_cfg_t task_cfg){}
+void CORE1_TASK98(task_cfg_t task_cfg){}
+void CORE1_TASK99(task_cfg_t task_cfg){}
+void CORE1_TASK100(task_cfg_t task_cfg){}
--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/Core2OS/core2_tasks.c
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/Core2OS/core2_tasks.c
@@ -15,144 +15,144 @@
 /****************************************************************************/
 /* <CORE2> Task Definitions                                                 */
 /****************************************************************************/
-void CORE2_TASK0(pthread_config_t *pthread_config)
+void CORE2_TASK0(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
-  App_shared_func_stack_background_count(pthread_config->curr_task_core_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
+  App_shared_func_stack_background_count(task_cfg->curr_task_core_id);
   App_priv2_func_test_count();
 
 }
-void CORE2_TASK1(pthread_config_t *pthread_config)
+void CORE2_TASK1(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
   //App_shared_func_flash_led_1();
 }
-void CORE2_TASK2(pthread_config_t *pthread_config)
+void CORE2_TASK2(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
   //App_shared_func_flash_led_2();
 }
-void CORE2_TASK3(pthread_config_t *pthread_config)
+void CORE2_TASK3(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE2_TASK4(pthread_config_t *pthread_config)
+void CORE2_TASK4(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE2_TASK5(pthread_config_t *pthread_config)
+void CORE2_TASK5(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE2_TASK6(pthread_config_t *pthread_config)
+void CORE2_TASK6(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE2_TASK7(pthread_config_t *pthread_config)
+void CORE2_TASK7(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE2_TASK8(pthread_config_t *pthread_config)
+void CORE2_TASK8(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE2_TASK9(pthread_config_t *pthread_config)
+void CORE2_TASK9(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
 }
-void CORE2_TASK10(pthread_config_t *pthread_config)
+void CORE2_TASK10(task_cfg_t task_cfg)
 {
-  App_shared_func_task_test_count(pthread_config->curr_task_core_id,pthread_config->curr_task_id);
-  App_shared_func_stack_calculated(pthread_config->curr_task_core_id);
+  App_shared_func_task_test_count(task_cfg->curr_task_core_id,task_cfg->curr_task_id);
+  App_shared_func_stack_calculated(task_cfg->curr_task_core_id);
   //App_shared_func_flash_led_4();
 }
-void CORE2_TASK11(pthread_config_t *pthread_config){}
-void CORE2_TASK12(pthread_config_t *pthread_config){}
-void CORE2_TASK13(pthread_config_t *pthread_config){}
-void CORE2_TASK14(pthread_config_t *pthread_config){}
-void CORE2_TASK15(pthread_config_t *pthread_config){}
-void CORE2_TASK16(pthread_config_t *pthread_config){}
-void CORE2_TASK17(pthread_config_t *pthread_config){}
-void CORE2_TASK18(pthread_config_t *pthread_config){}
-void CORE2_TASK19(pthread_config_t *pthread_config){}
-void CORE2_TASK20(pthread_config_t *pthread_config){}
-void CORE2_TASK21(pthread_config_t *pthread_config){}
-void CORE2_TASK22(pthread_config_t *pthread_config){}
-void CORE2_TASK23(pthread_config_t *pthread_config){}
-void CORE2_TASK24(pthread_config_t *pthread_config){}
-void CORE2_TASK25(pthread_config_t *pthread_config){}
-void CORE2_TASK26(pthread_config_t *pthread_config){}
-void CORE2_TASK27(pthread_config_t *pthread_config){}
-void CORE2_TASK28(pthread_config_t *pthread_config){}
-void CORE2_TASK29(pthread_config_t *pthread_config){}
-void CORE2_TASK30(pthread_config_t *pthread_config){}
-void CORE2_TASK31(pthread_config_t *pthread_config){}
-void CORE2_TASK32(pthread_config_t *pthread_config){}
-void CORE2_TASK33(pthread_config_t *pthread_config){}
-void CORE2_TASK34(pthread_config_t *pthread_config){}
-void CORE2_TASK35(pthread_config_t *pthread_config){}
-void CORE2_TASK36(pthread_config_t *pthread_config){}
-void CORE2_TASK37(pthread_config_t *pthread_config){}
-void CORE2_TASK38(pthread_config_t *pthread_config){}
-void CORE2_TASK39(pthread_config_t *pthread_config){}
-void CORE2_TASK40(pthread_config_t *pthread_config){}
-void CORE2_TASK41(pthread_config_t *pthread_config){}
-void CORE2_TASK42(pthread_config_t *pthread_config){}
-void CORE2_TASK43(pthread_config_t *pthread_config){}
-void CORE2_TASK44(pthread_config_t *pthread_config){}
-void CORE2_TASK45(pthread_config_t *pthread_config){}
-void CORE2_TASK46(pthread_config_t *pthread_config){}
-void CORE2_TASK47(pthread_config_t *pthread_config){}
-void CORE2_TASK48(pthread_config_t *pthread_config){}
-void CORE2_TASK49(pthread_config_t *pthread_config){}
-void CORE2_TASK50(pthread_config_t *pthread_config){}
-void CORE2_TASK51(pthread_config_t *pthread_config){}
-void CORE2_TASK52(pthread_config_t *pthread_config){}
-void CORE2_TASK53(pthread_config_t *pthread_config){}
-void CORE2_TASK54(pthread_config_t *pthread_config){}
-void CORE2_TASK55(pthread_config_t *pthread_config){}
-void CORE2_TASK56(pthread_config_t *pthread_config){}
-void CORE2_TASK57(pthread_config_t *pthread_config){}
-void CORE2_TASK58(pthread_config_t *pthread_config){}
-void CORE2_TASK59(pthread_config_t *pthread_config){}
-void CORE2_TASK60(pthread_config_t *pthread_config){}
-void CORE2_TASK61(pthread_config_t *pthread_config){}
-void CORE2_TASK62(pthread_config_t *pthread_config){}
-void CORE2_TASK63(pthread_config_t *pthread_config){}
-void CORE2_TASK64(pthread_config_t *pthread_config){}
-void CORE2_TASK65(pthread_config_t *pthread_config){}
-void CORE2_TASK66(pthread_config_t *pthread_config){}
-void CORE2_TASK67(pthread_config_t *pthread_config){}
-void CORE2_TASK68(pthread_config_t *pthread_config){}
-void CORE2_TASK69(pthread_config_t *pthread_config){}
-void CORE2_TASK70(pthread_config_t *pthread_config){}
-void CORE2_TASK71(pthread_config_t *pthread_config){}
-void CORE2_TASK72(pthread_config_t *pthread_config){}
-void CORE2_TASK73(pthread_config_t *pthread_config){}
-void CORE2_TASK74(pthread_config_t *pthread_config){}
-void CORE2_TASK75(pthread_config_t *pthread_config){}
-void CORE2_TASK76(pthread_config_t *pthread_config){}
-void CORE2_TASK77(pthread_config_t *pthread_config){}
-void CORE2_TASK78(pthread_config_t *pthread_config){}
-void CORE2_TASK79(pthread_config_t *pthread_config){}
-void CORE2_TASK80(pthread_config_t *pthread_config){}
-void CORE2_TASK81(pthread_config_t *pthread_config){}
-void CORE2_TASK82(pthread_config_t *pthread_config){}
-void CORE2_TASK83(pthread_config_t *pthread_config){}
-void CORE2_TASK84(pthread_config_t *pthread_config){}
-void CORE2_TASK85(pthread_config_t *pthread_config){}
-void CORE2_TASK86(pthread_config_t *pthread_config){}
-void CORE2_TASK87(pthread_config_t *pthread_config){}
-void CORE2_TASK88(pthread_config_t *pthread_config){}
-void CORE2_TASK89(pthread_config_t *pthread_config){}
-void CORE2_TASK90(pthread_config_t *pthread_config){}
-void CORE2_TASK91(pthread_config_t *pthread_config){}
-void CORE2_TASK92(pthread_config_t *pthread_config){}
-void CORE2_TASK93(pthread_config_t *pthread_config){}
-void CORE2_TASK94(pthread_config_t *pthread_config){}
-void CORE2_TASK95(pthread_config_t *pthread_config){}
-void CORE2_TASK96(pthread_config_t *pthread_config){}
-void CORE2_TASK97(pthread_config_t *pthread_config){}
-void CORE2_TASK98(pthread_config_t *pthread_config){}
-void CORE2_TASK99(pthread_config_t *pthread_config){}
-void CORE2_TASK100(pthread_config_t *pthread_config){}
+void CORE2_TASK11(task_cfg_t task_cfg){}
+void CORE2_TASK12(task_cfg_t task_cfg){}
+void CORE2_TASK13(task_cfg_t task_cfg){}
+void CORE2_TASK14(task_cfg_t task_cfg){}
+void CORE2_TASK15(task_cfg_t task_cfg){}
+void CORE2_TASK16(task_cfg_t task_cfg){}
+void CORE2_TASK17(task_cfg_t task_cfg){}
+void CORE2_TASK18(task_cfg_t task_cfg){}
+void CORE2_TASK19(task_cfg_t task_cfg){}
+void CORE2_TASK20(task_cfg_t task_cfg){}
+void CORE2_TASK21(task_cfg_t task_cfg){}
+void CORE2_TASK22(task_cfg_t task_cfg){}
+void CORE2_TASK23(task_cfg_t task_cfg){}
+void CORE2_TASK24(task_cfg_t task_cfg){}
+void CORE2_TASK25(task_cfg_t task_cfg){}
+void CORE2_TASK26(task_cfg_t task_cfg){}
+void CORE2_TASK27(task_cfg_t task_cfg){}
+void CORE2_TASK28(task_cfg_t task_cfg){}
+void CORE2_TASK29(task_cfg_t task_cfg){}
+void CORE2_TASK30(task_cfg_t task_cfg){}
+void CORE2_TASK31(task_cfg_t task_cfg){}
+void CORE2_TASK32(task_cfg_t task_cfg){}
+void CORE2_TASK33(task_cfg_t task_cfg){}
+void CORE2_TASK34(task_cfg_t task_cfg){}
+void CORE2_TASK35(task_cfg_t task_cfg){}
+void CORE2_TASK36(task_cfg_t task_cfg){}
+void CORE2_TASK37(task_cfg_t task_cfg){}
+void CORE2_TASK38(task_cfg_t task_cfg){}
+void CORE2_TASK39(task_cfg_t task_cfg){}
+void CORE2_TASK40(task_cfg_t task_cfg){}
+void CORE2_TASK41(task_cfg_t task_cfg){}
+void CORE2_TASK42(task_cfg_t task_cfg){}
+void CORE2_TASK43(task_cfg_t task_cfg){}
+void CORE2_TASK44(task_cfg_t task_cfg){}
+void CORE2_TASK45(task_cfg_t task_cfg){}
+void CORE2_TASK46(task_cfg_t task_cfg){}
+void CORE2_TASK47(task_cfg_t task_cfg){}
+void CORE2_TASK48(task_cfg_t task_cfg){}
+void CORE2_TASK49(task_cfg_t task_cfg){}
+void CORE2_TASK50(task_cfg_t task_cfg){}
+void CORE2_TASK51(task_cfg_t task_cfg){}
+void CORE2_TASK52(task_cfg_t task_cfg){}
+void CORE2_TASK53(task_cfg_t task_cfg){}
+void CORE2_TASK54(task_cfg_t task_cfg){}
+void CORE2_TASK55(task_cfg_t task_cfg){}
+void CORE2_TASK56(task_cfg_t task_cfg){}
+void CORE2_TASK57(task_cfg_t task_cfg){}
+void CORE2_TASK58(task_cfg_t task_cfg){}
+void CORE2_TASK59(task_cfg_t task_cfg){}
+void CORE2_TASK60(task_cfg_t task_cfg){}
+void CORE2_TASK61(task_cfg_t task_cfg){}
+void CORE2_TASK62(task_cfg_t task_cfg){}
+void CORE2_TASK63(task_cfg_t task_cfg){}
+void CORE2_TASK64(task_cfg_t task_cfg){}
+void CORE2_TASK65(task_cfg_t task_cfg){}
+void CORE2_TASK66(task_cfg_t task_cfg){}
+void CORE2_TASK67(task_cfg_t task_cfg){}
+void CORE2_TASK68(task_cfg_t task_cfg){}
+void CORE2_TASK69(task_cfg_t task_cfg){}
+void CORE2_TASK70(task_cfg_t task_cfg){}
+void CORE2_TASK71(task_cfg_t task_cfg){}
+void CORE2_TASK72(task_cfg_t task_cfg){}
+void CORE2_TASK73(task_cfg_t task_cfg){}
+void CORE2_TASK74(task_cfg_t task_cfg){}
+void CORE2_TASK75(task_cfg_t task_cfg){}
+void CORE2_TASK76(task_cfg_t task_cfg){}
+void CORE2_TASK77(task_cfg_t task_cfg){}
+void CORE2_TASK78(task_cfg_t task_cfg){}
+void CORE2_TASK79(task_cfg_t task_cfg){}
+void CORE2_TASK80(task_cfg_t task_cfg){}
+void CORE2_TASK81(task_cfg_t task_cfg){}
+void CORE2_TASK82(task_cfg_t task_cfg){}
+void CORE2_TASK83(task_cfg_t task_cfg){}
+void CORE2_TASK84(task_cfg_t task_cfg){}
+void CORE2_TASK85(task_cfg_t task_cfg){}
+void CORE2_TASK86(task_cfg_t task_cfg){}
+void CORE2_TASK87(task_cfg_t task_cfg){}
+void CORE2_TASK88(task_cfg_t task_cfg){}
+void CORE2_TASK89(task_cfg_t task_cfg){}
+void CORE2_TASK90(task_cfg_t task_cfg){}
+void CORE2_TASK91(task_cfg_t task_cfg){}
+void CORE2_TASK92(task_cfg_t task_cfg){}
+void CORE2_TASK93(task_cfg_t task_cfg){}
+void CORE2_TASK94(task_cfg_t task_cfg){}
+void CORE2_TASK95(task_cfg_t task_cfg){}
+void CORE2_TASK96(task_cfg_t task_cfg){}
+void CORE2_TASK97(task_cfg_t task_cfg){}
+void CORE2_TASK98(task_cfg_t task_cfg){}
+void CORE2_TASK99(task_cfg_t task_cfg){}
+void CORE2_TASK100(task_cfg_t task_cfg){}
