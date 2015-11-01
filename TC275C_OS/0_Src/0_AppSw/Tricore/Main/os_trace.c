091e2eab0c9de39044071365555bcf559c02b089
XiYanpeng <yanpeng.xi@delphi.com>
2015-11-01 10:11:05 +0800

    trace comment

--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/os_trace.c
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/os_trace.c
@@ -3,29 +3,52 @@
 /* CREATE ON:    Aug 26, 2015                                               */
 /* AUTHER:       Yanpeng.xi                                                 */
 /* DESCRIPTION:  The c file includes logic that traces the time             */
-/*               characteristics of tasks                                   */
+/*               characteristics of threads                                 */
 /*               tasks                                                      */
 /* COMMENT:      Multicore OS based on Aurix 275C app kit and TASKING 4.3   */
 /*               compiler                                                   */
 /****************************************************************************/
+
+/****************************************************************************/
+/* Feature Include Files                                                    */
+/****************************************************************************/
 #include "os_type.h"
 #include "low_driver_port.h"
 
+/****************************************************************************/
+/* Macro Definitions                                                        */
+/****************************************************************************/
 #define TRACE_MAX_CORE_NUM    (3)
 #define TRACE_MAX_THREAD_NUM  (11)
 
+/****************************************************************************/
+/* Global Variable Definitions                                              */
+/****************************************************************************/
+osu32_t os_thread_thread_timeslot[TRACE_MAX_CORE_NUM];
+
+/****************************************************************************/
+/* Static Variable Definitions                                              */
+/****************************************************************************/
 static volatile osu32_t os_thread_curr_exe_time_rec[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
 static volatile osu32_t os_thread_max_exe_time_rec[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
 static volatile osu32_t os_thread_begin_timeslot_rec[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
 static volatile osu32_t os_thread_end_timeslot_rec[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
 static volatile osu32_t os_thread_curr_switching_times[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
 static volatile osu32_t os_thread_max_switching_times[TRACE_MAX_CORE_NUM][TRACE_MAX_THREAD_NUM];
-osu32_t os_thread_thread_timeslot[TRACE_MAX_CORE_NUM];
 
+/****************************************************************************/
+/* FUNTION NAME:os_trace_time                                               */
+/* DESCRIPTION: Get time slot by call lowdriver API                         */
+/****************************************************************************/
 static osu32_t os_trace_time(void)
 {
-  return (osu32_t)LowDriver_GetStmLower_Count();
-}
+    return (osu32_t)LowDriver_GetStmLower_Count();
+} /* End of os_trace_time function  */
+
+/****************************************************************************/
+/* FUNTION NAME:os_trace_task_time_begin                                    */
+/* DESCRIPTION: Trace logic that is set up before each task in thread       */
+/****************************************************************************/
 void os_trace_task_time_begin(osu32_t curr_core_id,osu32_t thread_id)
 {  
     if(thread_id < TRACE_MAX_THREAD_NUM)
@@ -43,7 +66,12 @@ void os_trace_task_time_begin(osu32_t curr_core_id,osu32_t thread_id)
 		  os_thread_curr_switching_times[curr_core_id][thread_id];	
 	  }
 	}
-}
+} /* End of os_trace_time function  */
+
+/****************************************************************************/
+/* FUNTION NAME:os_trace_task_time_begin                                    */ 
+/* DESCRIPTION: Trace logic that is set up before each task in thread       */
+/****************************************************************************/
 void os_trace_task_time_end(osu32_t curr_core_id,osu32_t thread_id)
 { 
 	if(thread_id < TRACE_MAX_THREAD_NUM)
@@ -65,7 +93,7 @@ void os_trace_task_time_end(osu32_t curr_core_id,osu32_t thread_id)
 	 	  os_thread_curr_exe_time_rec[curr_core_id][thread_id];
 	  }
 	}
-}
+}/* End of os_trace_task_time_end function  */
 
 
 
--- TC275C_OS/0_Src/0_AppSw/Tricore/Main/os_trace.h
+++ TC275C_OS/0_Src/0_AppSw/Tricore/Main/os_trace.h
@@ -2,17 +2,18 @@
 /* FILE NAME:    os_trace.h                                                 */
 /* CREATE ON:    Aug 26, 2015                                               */
 /* AUTHER:       Yanpeng.xi                                                 */
-/* DESCRIPTION:  The h file includes logic that traces the time             */
-/*               characteristics of tasks                                   */
+/* DESCRIPTION:  The h file includes function prototype definitions of trace*/
 /*               tasks                                                      */
 /* COMMENT:      Multicore OS based on Aurix 275C app kit and TASKING 4.3   */
 /*               compiler                                                   */
 /****************************************************************************/
-
 #ifndef OS_TRACE_H_
 #define OS_TRACE_H_
 
+/****************************************************************************/
+/* Function Prototype Definitions                                           */
+/****************************************************************************/
 extern void os_trace_task_time_begin(osu32_t curr_core_id,osu32_t thread_id);
 extern void os_trace_task_time_end(osu32_t curr_core_id,osu32_t thread_id);
 
-#endif
+#endif /* End of OS_TRACE_H_ */
